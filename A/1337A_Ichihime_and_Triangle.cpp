0b55ffb1618f9481b76602f694da193e40f9bccd
Mukund Kansara <mukundkansara2411@gmail.com>
2025-01-21 19:44:38 +0530

    Create Solution of 1337A (Ichihime and Triangle)

---
+++ A/1337A_Ichihime_and_Triangle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << b << " " << c << " " << c << " " << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        solve();
+    }
+    return 0;
+}
