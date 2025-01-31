a2e89bcf6e027075a9cdcc9f968b0a1336fb116d
Mukund Kansara <mukundkansara2411@gmail.com>
2025-01-31 12:33:03 +0530

    Create Solution of 1869A (Make It Zero)

---
+++ A/1869A_Make_It_Zero.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    if (n & 1)
+    {
+        cout << 4 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
+    }
+    else
+    {
+        cout << 2 << endl;
+        cout << 1 << " " << n << endl;
+        cout << 1 << " " << n << endl;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    while (n--)
+    {
+        solve();
+    }
+    return 0;
+}
